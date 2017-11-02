01e6ef55906eb304a1d8a47f11c694bded6eeee7
pedrom40 <pedro.morin1980@gmail.com>
2017-11-02 10:21:15 -0500

    added ability to add, edit and delete admin users

--- public/js/admin.js
+++ public/js/admin.js
@@ -253,112 +253,265 @@ function loadAdminDashboard () {
 // users
 function loadAdminUsers () {
 
-    // setup member display
-    callUserService({method: 'getMembers'})
-      .then( members => {
+  // setup member display
+  callUserService({method: 'getMembers'})
+    .then( members => {
 
-        // markup order rows
-        let memberRowsHtml = '';
-        members.map( member => {
-          memberRowsHtml = `${memberRowsHtml}
-            <tr>
-              <td>${member[0]}</td>
-              <td>${member[1]}</td>
-              <td>${member[2]}</td>
-              <td>${member[3]}</td>
-            </tr>
+      // markup order rows
+      let memberRowsHtml = '';
+      members.map( member => {
+        memberRowsHtml = `${memberRowsHtml}
+          <tr>
+            <td>${member[0]}</td>
+            <td>${member[1]}</td>
+            <td>${member[2]}</td>
+            <td>${member[3]}</td>
+          </tr>
+        `;
+      });
+
+      // markup admin rows
+      let adminRowsHtml = '';
+      callUserService({method: 'getAdminUsers'})
+        .then( adminUsers => {
+
+          adminUsers.map( adminUser => {
+            adminRowsHtml = `${adminRowsHtml}
+              <tr>
+                <td>${adminUser[1]}</td>
+                <td class="admin-users-options">
+                  <a href="#" title="Edit User"><i id="admin-edit-btn_${adminUser[0]}" class="fa fa-pencil" aria-hidden="true"></i></a> |
+                  <a href="#" title="Delete User"><i id="admin-delete-btn_${adminUser[0]}" class="fa fa-trash" aria-hidden="true"></i></a>
+                </td>
+              </tr>
+            `;
+          });
+
+          // start fresh
+          $('.js-admin-placeholder').empty();
+
+          // markup search form
+          const memberSearchForm = `
+            <div class="row">
+              <div class="column">
+                <label for="memberSearchTerm" class="sr-only">Member Search Term</label>
+                <input type="text" id="memberSearchTerm" placeholder="Search by Name, Email or Phone">
+              </div>
+            </div>
           `;
+
+          // markup admin form
+          const adminForm = `
+            <form class="js-admin-user-form">
+
+              <label for="adminName">Name:</label>
+              <input type="text" id="adminName" placeholder="New Guy" required>
+
+              <label for="adminEmail">Email:</label>
+              <input type="email" id="adminEmail" placeholder="new@guy.com" required>
+
+              <label for="adminPassword">Password:</label>
+              <input type="password" id="adminPassword" placeholder="******" required>
+
+              <input type="submit" id="adminSubmit" value="Add">
+              <input type="hidden" id="adminId">
+
+            </form>
+          `;
+
+          // markup display
+          const displayHtml = `
+            <div class="row">
+              <div class="column">
+                <h4>Members</h4>
+
+                ${memberSearchForm}
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Customer</th>
+                      <th>Email</th>
+                      <th>Phone</th>
+                      <th>Orders</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    ${memberRowsHtml}
+                  </tbody>
+                </table>
+              </div>
+              <div class="column column-10"></div>
+              <div class="column">
+                <h4>Admin</h4>
+                <table class="js-admin-table">
+                  <tbody>
+                    ${adminRowsHtml}
+                  </tbody>
+                </table>
+
+                <br><h4 class="js-admin-form-title">Add Admin</h4>
+                ${adminForm}
+              </div>
+            </div>
+          `;
+
+          // load markup to page
+          $('.js-admin-placeholder').html(displayHtml);
+
         });
 
-        // markup admin rows
-        let adminRowsHtml = '';
-        callUserService({method: 'getAdminUsers'})
-          .then( adminUsers => {
+    });
 
-            adminUsers.map( adminUser => {
-              adminRowsHtml = `${adminRowsHtml}
-                <tr>
-                  <td><a href="#" id="admin-edit-btn_${adminUser[0]}">${adminUser[1]}</a></td>
-                  <td><a href="#" id="admin-delete-btn_${adminUser[0]}"><i class="fa fa-trash" aria-hidden="true"></i></a></td>
-                </tr>
-              `;
-            });
+  // setup listeners
+  listenForAdminActions();
 
-            // start fresh
-            $('.js-admin-placeholder').empty();
+}
 
-            // markup search form
-            const memberSearchForm = `
-              <div class="row">
-                <div class="column">
-                  <label for="memberSearchTerm" class="sr-only">Member Search Term</label>
-                  <input type="text" id="memberSearchTerm" placeholder="Search by Name, Email or Phone">
-                </div>
-              </div>
-            `;
+// listens for admin user edit/delete btn clicks and admin form submits
+function listenForAdminActions () {
 
-            // markup admin form
-            const adminForm = `
-              <form class="js-add-admin-user">
+  $('.js-admin-placeholder').click( event => {
+    event.preventDefault();
 
-                <label for="adminName">Name:</label>
-                <input type="text" id="adminName" placeholder="New Guy">
+    // for admin user edit click
+    if (event.target.id.search('admin-edit-btn') !== -1) {
+      const adminUserId = event.target.id.toString().split('_');
+      editAdminUser(adminUserId[1]);
+    }
 
-                <label for="adminEmail">Email:</label>
-                <input type="email" id="adminEmail" placeholder="new@guy.com">
+    // if admin user delete click
+    else if (event.target.id.search('admin-delete-btn') !== -1) {
+      const adminUserId = event.target.id.toString().split('_');
+      deleteAdminUser(adminUserId[1]);
+    }
 
-                <label for="adminPassword">Password:</label>
-                <input type="password" id="adminPasword" placeholder="******">
+    // for admin form submits
+    else if (event.target.id === 'adminSubmit') {
+      handleAdminUserFormSubmit();
+    }
 
-                <input type="submit" id="adminSubmit" value="Add">
+  });
 
-              </form>
-            `;
+}
 
-            // markup display
-            const displayHtml = `
-              <div class="row">
-                <div class="column">
-                  <h4>Members</h4>
+// edit admin user
+function editAdminUser (adminUserId) {
 
-                  ${memberSearchForm}
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Customer</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Orders</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      ${memberRowsHtml}
-                    </tbody>
-                  </table>
-                </div>
-                <div class="column column-10"></div>
-                <div class="column">
-                  <h4>Admin</h4>
-                  <table>
-                    <tbody>
-                      ${adminRowsHtml}
-                    </tbody>
-                  </table>
+  // get user info
+  callUserService({
+    method: 'getAdminUser',
+    adminUserId: adminUserId
+  })
+    .then( adminUser => {
 
-                  <h4>Add Admin</h4>
-                  ${adminForm}
-                </div>
-              </div>
-            `;
+      // update form content
+      $('#adminId').val(adminUser[0]);
+      $('#adminName').val(adminUser[1]);
+      $('#adminEmail').val(adminUser[2]);
 
-            // load markup to page
-            $('.js-admin-placeholder').html(displayHtml);
+      // change for title and submit text
+      $('.js-admin-form-title').html('Edit Admin');
+      $('#adminSubmit').val('EDIT');
 
-          });
+    });
+
+}
+
+// delete admin user
+function deleteAdminUser (adminUserId) {
+
+  // confirm delete
+  const confirmDelete = confirm('Delete this Admin User?');
+
+  // on confirm
+  if (confirmDelete) {
+
+    // get user info
+    callUserService({
+      method: 'deleteAdminUser',
+      adminUserId: adminUserId
+    })
+      .then( response => {
+
+        // if successful
+        if (response === 'success') {
+          window.location.assign('/admin/users');
+        }
+
+        // if not
+        else {
+          showErrorMsg(response);
+        }
+
+      });
+  }
+
+}
+
+// handle admin user form submit
+function handleAdminUserFormSubmit () {
+
+  const formAction = $('#adminSubmit').val();
+
+  // add admin user
+  if (formAction === 'Add') {
+
+    // send info to service
+    callUserService({
+      method: 'addAdminUser',
+      adminName: $('#adminName').val(),
+      adminEmail: $('#adminEmail').val(),
+      adminPassword: $('#adminPassword').val()
+    })
+      .then( response => {
+
+        // if successful, reload page
+        if (response === 'success') {
+          window.location.assign('/admin/users');
+        }
+
+        // if not
+        else {
+
+          // display error
+          showErrorMsg(response);
+
+        }
 
       });
 
   }
+
+  // edit admin user
+  else {
+
+    // send info to service
+    callUserService({
+      method: 'editAdminUser',
+      adminId: $('#adminId').val(),
+      adminName: $('#adminName').val(),
+      adminEmail: $('#adminEmail').val(),
+      adminPassword: $('#adminPassword').val()
+    })
+      .then( response => {
+
+        // if successful, reload page
+        if (response === 'success') {
+          window.location.assign('/admin/users');
+        }
+
+        // if not
+        else {
+
+          // display error
+          showErrorMsg(response);
+
+        }
+
+      });
+  }
+
+}
 
 // makes all calls to admin service
 function callAdminService (data) {
